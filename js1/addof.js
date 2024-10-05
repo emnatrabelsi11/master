a17f85954f5ab7028d2def379e3fb858474913f0
emnatrabelsi11 <163533468+emnatrabelsi11@users.noreply.github.com>
2024-10-05 01:05:51 +0100

    Add files via upload

---
+++ js1/addof.js
@@ -0,0 +1,29 @@
+const form=document.getElementsByClassName("propos")
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+    const titre= document.getElementById("txt").value;
+    const destination=new Date ( document.getElementById("des").value);
+    const ReturnDate = new Date(document.getElementById("dt").value);
+    const Price = document.getElementById("p").valure;
+    const checkout = document.getElementById("A").checked;
+    const category =  document.getElementById("cat").value;
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
